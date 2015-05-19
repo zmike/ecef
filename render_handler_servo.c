7acb09e9ab96acc3cc6f8ee6e8e6c70f0344ea18
Mike Blumenkrantz <zmike@osg.samsung.com>
2015-05-19 13:40:13 -0400

    use composite() method for servo rendering

--- render_handler_servo.c
+++ render_handler_servo.c
@@ -2,33 +2,14 @@
 
 #ifdef HAVE_SERVO
 static void
-render_image_servo_compositing_initialize(void *d)
-{
-   Browser *b = d;
-   cef_browser_host_t *host;
-
-   assert(evas_gl_make_current(b->gl, NULL, NULL));
-   assert(evas_gl_make_current(b->gl, b->gl_surf, b->gl_ctx));
-   host = browser_get_host(b->browser);
-   host->initialize_compositing(host);
-   b->gl_init = NULL;
-}
-
-static void
 render_image_servo_init(Evas_Object *obj)
 {
-   Evas_GL *gl;
    Browser *b;
+   cef_browser_host_t *host;
 
    b = evas_object_data_get(obj, "Browser");
-   b->gl = gl = elm_glview_evas_gl_get(b->img);
-   b->gl_cfg = evas_gl_config_new();
-   b->gl_cfg->color_format = EVAS_GL_RGB_888;
-   b->gl_cfg->depth_bits = EVAS_GL_DEPTH_BIT_24;
-   b->gl_cfg->stencil_bits = EVAS_GL_STENCIL_BIT_8;
-   b->gl_surf = evas_gl_surface_create(gl, b->gl_cfg, b->w, b->h);
-   b->gl_ctx = evas_gl_context_create(gl, evas_gl_current_context_get(gl));
-   b->gl_init = ecore_job_add(render_image_servo_compositing_initialize, b);
+   host = browser_get_host(b->browser);
+   host->initialize_compositing(host);
 }
 
 void
@@ -55,17 +36,21 @@ render_image_servo_present(cef_render_handler_t *handler, cef_browser_t *browser
    //evas_object_show(img);
 }
 
+static void
+render_image_servo_render(Evas_Object *obj)
+{
+   Browser *b;
+   cef_browser_host_t *host;
+
+   b = evas_object_data_get(obj, "Browser");
+   host = browser_get_host(b->browser);
+   host->composite(host);
+}
+
 void
 render_image_servo_paint(Browser *b)
 {
-   evas_gl_make_current(b->gl, NULL, NULL);
-   if ((b->w != b->pw) || (b->h != b->ph))
-     {
-        evas_gl_surface_destroy(b->gl, b->gl_surf);
-        b->gl_surf = evas_gl_surface_create(b->gl, b->gl_cfg, b->w, b->h);
-        elm_glview_size_set(b->img, b->w, b->h);
-     }
-   evas_gl_make_current(b->gl, b->gl_surf, b->gl_ctx);
+   elm_glview_size_set(b->img, b->w, b->h);
 }
 
 void
@@ -75,7 +60,7 @@ render_image_servo_setup(Browser *b, int w, int h)
    elm_glview_init_func_set(b->img, render_image_servo_init);
    elm_glview_resize_policy_set(b->img, ELM_GLVIEW_RESIZE_POLICY_SCALE);
    /* force setup of internal render callbacks because glview is a stupid widget */
-   elm_glview_render_func_set(b->img, NULL);
+   elm_glview_render_func_set(b->img, render_image_servo_render);
    elm_glview_size_set(b->img, w, h);
    elm_glview_changed_set(b->img);
 }
