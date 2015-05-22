4f502d05a2462167c24eeb49cda23603233d389a
Mike Blumenkrantz <zmike@osg.samsung.com>
2015-05-21 22:25:39 -0400

    only send unicode key when key event passes a string to convert

--- render_handler.c
+++ render_handler.c
@@ -241,7 +241,7 @@ render_image_key(ECef_Client *ec, Evas *e, Evas_Object *obj, Evas_Event_Key_Down
       // We need to treat the enter key as a key press of character \r.  This
       // is apparently just how webkit handles it and what it expects.
       event.unmodified_character = '\r';
-   else
+   else if (ev->string)
      {
         // FIXME: fix for non BMP chars
         str = eina_unicode_utf8_to_unicode(ev->string, NULL);
