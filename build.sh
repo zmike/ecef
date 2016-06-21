99b8ceaeb0c690c7a8fe8b4b138316ced493135b
Mike Blumenkrantz <zmike@osg.samsung.com>
2016-06-21 14:58:56 -0400

    remove bad linker path

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ CEF_LIB_DIR=
 edje_cc -id icons-dd ecef.edc &
 
 
-gcc -o ecef -DHAVE_SERVO $(ls *.c|grep -v test.c) $(pkg-config --cflags --libs elementary ecore-x evas) -I.  -g -O0 -Wall -lcef -L. -L../servo
+gcc -o ecef -DHAVE_SERVO $(ls *.c|grep -v test.c) $(pkg-config --cflags --libs elementary ecore-x evas) -I.  -g -O0 -Wall -lcef -L.
 #gcc -o ecef *.c $(pkg-config --cflags --libs elementary ecore-x evas) -I ../cef-bin/cef_binary_3.2321.2067_linux64/  -g -O0 -Wall -lcef -L../cef-bin/cef_binary_3.2321.2067_linux64/Release -lGL
 #gcc -o ecef *.c $(pkg-config --cflags --libs elementary ecore-x evas) -I ../cef-bin/cef_binary_3.2217.1922_linux64/  -g -O0 -Wall -lcef -L../cef-bin/cef_binary_3.2217.1922_linux64/out/Release -lGL
 #gcc -o ecef *.c $(pkg-config --cflags --libs elementary ecore-x evas) -I ../cef-bin/cef_binary_3.2171.1901_linux64/  -g -O0 -Wall -lcef -L../cef-bin/cef_binary_3.2171.1901_linux64/out/Release -lGL
